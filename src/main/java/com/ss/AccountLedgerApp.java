6c704ce3a019afdaca2c7409d173ae5dc9c8269a
Salha Siddig <129996035+ssiddig@users.noreply.github.com>
2023-05-01 12:54:17 -0400

    "First Commit"

--- src/main/java/com/ss/AccountLedgerApp.java
+++ src/main/java/com/ss/AccountLedgerApp.java
@@ -1,4 +1,32 @@
 package com.ss;
 
+//create scanner class
+import java.util.Scanner;
+
+//create Array list
+import java.util.ArrayList;
 public class AccountLedgerApp {
+    public static void main(String[] args){
+
+        //initialize scanner
+        Scanner scanner = new Scanner(System.in);
+
+        // Create static variable called Entries . It will be an ArrayList of (Dates, payments, deposits)
+        // Create static scanner instance
+
+        // Pseudocode code:
+        // Ledger: All entries should show the newest entries
+        // (A) All- Display all entries
+        // (D) Deposits-Display only the entries that are deposits into the account
+        // (P) Payments-Display only the negative entries (or payments)
+        // (R) Reports- A new screen that allows the user to run pre-defined reports or to run a custom  search
+        // 1) Month to Date
+        // 2) Previous Month
+        // 3) Year To Date
+        // 4) Previous Year
+        // 5) Search By Vendor-prompt the user the vendor name and display all entries for that vendor
+        // 0) Back-go back to the report page
+        // H) Home-go back to the home page
+
+    }
 }
